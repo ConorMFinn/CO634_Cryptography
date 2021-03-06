0c3ab4eb5c293ea6ac4617dd7ecacda71282b670
Conor Finn <cf338@kent.ac.uk>
2021-03-06 17:23:55 +0000

    Add comments to caesar decryptor

--- Scripts/caesar.py
+++ Scripts/caesar.py
@@ -1,10 +1,15 @@
+# Author: Conor Finn (CF338)
+# https://github.com/ConorMFinn/CO634_Cryptography
+
 import string
 
+# Pull text from input
 text = input()
 text = text.upper()
 
 alphabet = string.ascii_uppercase
 
+# Count letters in the ciphertext
 counts = {}
 
 for letter in alphabet:
@@ -13,6 +18,7 @@ for letter in alphabet:
 for char in text:
     counts[char] += 1
 
+# Find the most common letter in the ciphertext
 largest_letter = ""
 largest_count = 0
 
@@ -23,6 +29,9 @@ for letter in alphabet:
 
 print(largest_letter)
 
+# Assume the most common letter is one of the most common three 
+# in the english language (E, T, and A), then shift the text on 
+# these assumtions.
 shift_e = alphabet.index(largest_letter) - alphabet.index("E")
 shift_t = alphabet.index(largest_letter) - alphabet.index("T")
 shift_a = alphabet.index(largest_letter) - alphabet.index("A")
@@ -40,6 +49,8 @@ decode_a = ""
 for char in text:
     decode_a += alphabet[(alphabet.index(char) - shift_a) % 26]
 
+
+# Print all three, determine the real plaintext manually. 
 print("E: \n" + decode_e + "\n")
 print("T: \n" + decode_t + "\n")
 print("A: \n" + decode_a + "\n")
