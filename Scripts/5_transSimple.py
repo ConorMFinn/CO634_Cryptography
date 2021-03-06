5859aaf74cd77eb5f770271032319fed287f373d
Conor Finn <cf338@kent.ac.uk>
2021-03-06 20:00:35 +0000

    Add submission for EX5

--- Scripts/5_transSimple.py
+++ Scripts/5_transSimple.py
@@ -5,12 +5,14 @@ CY_LEN = 4
 
 splits = int(len(text)/CY_LEN)
 
+# Split ciphertext into the columns used to create the cipher
 sections = [text[i:i+splits] for i in range(0, len(text), splits)]
 
 print(sections)
 
 plaintext = ""
 
+# Turn the columms back into the plaintext
 for i in range(len(text)):
     plaintext += sections[int(i%4)][int(i/4)]
 
