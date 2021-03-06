cd5926e229cf2674d147335f4e41316fb31f6f5f
Conor Finn <cf338@kent.ac.uk>
2021-03-06 18:47:59 +0000

    Add EX2 submission info

--- Scripts/2_vigenere.py
+++ Scripts/2_vigenere.py
@@ -1,14 +1,17 @@
 import string
 
+# Take ciphertext from CLI
 ciphertext = input()
 ciphertext = ciphertext.upper()
 
 alphabet = string.ascii_uppercase
 
+# Key given by exercise
 key = "TESSOFTHEDURBERVILLES"
 
 plaintext = ""
 
+# Use key to decrypt using position in alphabet of key letter to shift each ciphertext letter.
 for i in range(0, len(ciphertext)):
     plaintext += alphabet[(alphabet.index(ciphertext[i]) - alphabet.index(key[i%len(key)])) % 26]
 
