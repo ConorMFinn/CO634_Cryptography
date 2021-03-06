c7440e0b53be7c9bbe83cd6cdf6aac1347e1be85
Conor Finn <cf338@kent.ac.uk>
2021-03-06 19:21:18 +0000

    Add EX3 explanation & comments

--- Scripts/3_6letterkey_vigenere.py
+++ Scripts/3_6letterkey_vigenere.py
@@ -17,7 +17,7 @@ key2 = ""
 
 
 for section in ciphers:
-    # Generate alphabet dict
+    # Find the most common letter in the ciphertext
     counts = {}
     for letter in alphabet:
         counts[letter] = 0
@@ -33,14 +33,17 @@ for section in ciphers:
             largest_count = counts[letter]
             largest_letter = letter
 
+    # Generate keys based on the two most common letters
     key += alphabet[(alphabet.index(largest_letter) - alphabet.index("E")) % 26]
     key2 += alphabet[(alphabet.index(largest_letter) - alphabet.index("T")) % 26]
 
 print(key)
 print(key2)
 
+# Find all the combinations of those two keys
 combinations = [''.join(s) for s in itertools.product(*zip(key,key2))]
 
+# Create a plaintext for each of the combined keys
 plaintexts = []
 for test_key in combinations:
     plaintext = ""
@@ -49,8 +52,10 @@ for test_key in combinations:
     plaintext = test_key + ":\n" + plaintext + "\n"
     plaintexts.append(plaintext)
 
+# The more "THE"s are found, the more likely it's the true plaintext
 plaintexts.sort(key=lambda x: x.count('THE'))
 
+# Find the correct plaintext manually. 
 for plaintext in plaintexts:
     print(plaintext)
 
